5de2b5f72aa0bfaf4a4c93c46a6635184f91f0a4
allantestaten <112443674+allantestaten@users.noreply.github.com>
2022-11-20 07:01:54 -0600

    updated model

--- model.py
+++ model.py
@@ -5,64 +5,149 @@ from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets
 from sklearn.model_selection import train_test_split
 
-
-def train_validate_test_split(df, target, seed=123):
+def train_validate_test_split(df, seed=123):
     '''
-    This function takes in a dataframe, the name of the target variable
-    (for stratification purposes), and an integer for a setting a seed
-    and splits the data into train, validate and test. 
-    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
-    original dataset, and train is .70*.80= 56% of the original dataset. 
-    The function returns, in this order, train, validate and test dataframes. 
+    This function takes in a dataframe and an integer for a setting a seed
+    and splits the data into train, validate and test. The function returns, 
+    in this order, train, validate and test dataframes. 
     '''
-    train_validate, test = train_test_split(df, test_size=0.2, 
-                                            random_state=seed, 
-                                            stratify=df[target])
-    train, validate = train_test_split(train_validate, test_size=0.3, 
-                                       random_state=seed,
-                                       stratify=train_validate[target])
-    return train, validate, test
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test  
 
+def scale():
+    # assigning features to x/y train, validate and test 
+    X_train = train[x_cols]
+    y_train = train['property_value']
 
-def map_setosa_knn(X_train, y_train, knn, fig_x=12.0, fig_y=5.0):
-    plt.rcParams["figure.figsize"] = [fig_x, fig_y]
-    plt.rcParams["figure.autolayout"] = True
+    X_validate = validate[x_cols]
+    y_validate = validate['property_value']
 
-    n_neighbors = knn.get_params()['n_neighbors']
-    weights = knn.get_params()['weights']
+    X_test = test[x_cols]
+    y_test = test['property_value']
 
-    iris = datasets.load_iris()
-    X = np.array(X_train[['sepal_length', 'sepal_width']])
-    y = y_train.map({'setosa':0, 'versicolor':1, 'virginica':2})
-    h = .02
+    # applying scaling to all the data splits.
+    scaler = sklearn.preprocessing.RobustScaler()
+    scaler.fit(X_train)
 
-    cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
-    cmap_bold = ['c', 'darkorange', 'darkblue']
+    # transforming train, validate and test datasets
+    X_train_scaled = scaler.transform(X_train)
+    X_validate_scaled = scaler.transform(X_validate)
+    X_test_scaled = scaler.transform(X_test)
 
-    clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
-    clf.fit(X, y)
+def baseline_model():
+    ''' this model is creating and printing the baseline RMSE'''
+    # We need y_train and y_validate to be dataframes to append the new columns with predicted values. 
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
 
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-    np.arange(y_min, y_max, h))
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
+    # 1. Predict property_value_pred_mean
+    prop_value_pred_mean = y_train['property_value'].mean()
+    y_train['prop_value_pred_mean'] = prop_value_pred_mean
+    y_validate['prop_value_pred_mean'] = prop_value_pred_mean
 
-    plt.figure()
+    # 2. compute prop_value_pred_median
+    prop_value_pred_median = y_train['property_value'].median()
+    y_train['prop_value_pred_median'] = prop_value_pred_median
+    y_validate['prop_value_pred_median'] = prop_value_pred_median
 
-    plt.contourf(xx, yy, Z, cmap=cmap_light)
+    # 3. RMSE of prop_value_pred_median
+    rmse_baseline_train = mean_squared_error(y_train.property_value, y_train.prop_value_pred_median)**(1/2)
+    rmse_baseline_validate = mean_squared_error(y_validate.property_value, y_validate.prop_value_pred_median)**(1/2)
 
-    sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=iris.target_names[y],
-    palette=cmap_bold, alpha=1.0, edgecolor="black")
+    print("RMSE using Median\nTrain/In-Sample: ", round(rmse_train, 2), 
+      "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
 
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
+def linearOLS(X_train, y_train):
+    '''this function provides the results of the linear regression model'''
+    # create the model object
+    lm = LinearRegression(normalize=True)
 
-    plt.title("3-Class classification (k = %i, '%s')"
-    % (n_neighbors, weights))
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm.fit(X_train, y_train.property_value)
 
-    plt.xlabel(iris.feature_names[0])
-    plt.ylabel(iris.feature_names[1])
+    # predict train
+    y_train['property_value_pred_lm'] = lm.predict(X_train)
 
-    plt.show()
+    # evaluate: rmse
+    rmse_linearreg_train = mean_squared_error(y_train.property_value, y_train.property_value_pred_lm)**(1/2)
+
+    # predict validate
+    y_validate['property_value_pred_lm'] = lm.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_linearreg_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_lm)**(1/2)
+
+    print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate,"\nPerformance Compared to Baseline for OLS using LinearRegression\nTraining/In-Sample: ", difference)
+
+def lassoLars():
+    # create the model object
+    lars = LassoLars(alpha=1.0)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lars.fit(X_train, y_train.property_value)
+
+    # predict train
+    y_train['property_value_pred_lars'] = lars.predict(X_train)
+
+    # evaluate: rmse
+    rmse_laso_lars_train = mean_squared_error(y_train.property_value, y_train.property_value_pred_lars)**(1/2)
+
+    # predict validate
+    y_validate['property_value_pred_lars'] = lars.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_laso_lars_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_lars)**(1/2)
+
+    print("RMSE for Lasso + Lars\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+
+def tweedie():
+    # create the model object
+    glm = TweedieRegressor(power=1, alpha=0)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    glm.fit(X_train, y_train.property_value)
+
+    # predict train
+    y_train['property_value_pred_glm'] = glm.predict(X_train)
+
+    # evaluate: rmse
+    rmse_tweedie_train = mean_squared_error(y_train.property_value, y_train.property_value_pred_glm)**(1/2)
+
+    # predict validate
+    y_validate['property_value_pred_glm'] = glm.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_tweedie_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_glm)**(1/2)
+
+    print("RMSE for GLM using Tweedie, power=1 & alpha=0\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)    
+
+def linearReg():
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, y_train.property_value)
+
+    # predict train
+    y_train['property_value_pred_lm2'] = lm2.predict(X_train_degree2)
+
+    # evaluate: rmse
+    rmse_linear_train = mean_squared_error(y_train.property_value, y_train.property_value_pred_lm2)**(1/2)
+
+    # predict validate
+    y_validate['property_value_pred_lm2'] = lm2.predict(X_validate_degree2)
+
+    # evaluate: rmse
+    rmse_linear_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_lm2)**(1/2)
+
+    print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
